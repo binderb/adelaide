f6f188046872516f03e3bcf3efbf07aa4f8a5e27
Benjamin Binder <binder@bkp-bbinder-lmo.lan>
2024-04-07 23:28:04 -0500

    Modify site metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Adelaide",
   description: "Baby's first Next.js app",
+  generator: "Next.js",
+  manifest: "/manifest.webmanifest",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Ben Binder" },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/icon-128x128.png" },
+    { rel: "icon", url: "/icon-128x128.png" },
+  ],
 };
 
 export default async function RootLayout({
