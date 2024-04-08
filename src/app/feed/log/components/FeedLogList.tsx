b27507d0095f45f63eea08168a3a47a63bc80705
Benjamin Binder <binder@bkp-bbinder-lmo.lan>
2024-04-07 22:54:45 -0500

    Adjust feed log display, add PWA manifest.

--- src/app/feed/log/components/FeedLogList.tsx
+++ src/app/feed/log/components/FeedLogList.tsx
@@ -1,35 +1,47 @@
 'use client';
 
-import { FaTrashAlt } from "react-icons/fa";
+import { FaEdit, FaPen, FaTrashAlt } from "react-icons/fa";
 import { deleteFeed } from "../actions";
 import { TrackingData } from "@/db/schema_trackerModule";
 
 type Props = {
-  feeds: TrackingData[]
-}
+  feeds: TrackingData[];
+};
 
 export default function FeedLogList({ feeds }: Props) {
+
+  async function handleDeleteFeed(formData: FormData) {
+    if (confirm('Are you sure you want to delete this feed?')) {
+      await deleteFeed(formData);
+    }
+  }
+
   return (
     <section className="w-full flex flex-col items-center justify-start gap-4 pt-4">
-        <ul className='w-full flex flex-col items-center justify-start gap-2 px-6 text-sm'>
-          {feeds.map((feed) => (
-            <li key={feed.id} className='w-full flex items-center justify-between gap-1 bg-primary/50 px-2 py-1 rounded-md'>
-              
-              <p>
-                {/* show date and time without seconds */}
-                {feed.timestamp!.toLocaleString('en-US', { dateStyle: 'short', timeStyle: 'short' })}
-              </p>
-              <p>{feed.type}</p>
-              <p>{Math.floor(feed.length! / 60)} minutes, {feed.length! % 60} seconds</p>
-              <form action={deleteFeed}>
+      <ul className='w-full flex flex-col items-center justify-start gap-2 px-6 text-sm'>
+        {feeds.map((feed) => (
+          <li key={feed.id} className='w-full flex items-center justify-between gap-1 bg-primary/50 px-2 py-1 rounded-md'>
+
+            <p className='whitespace-pre'>
+              {/* show date and time without seconds */}
+              {feed.timestamp!.toLocaleString('en-US', { dateStyle: 'short', timeStyle: 'short' }).replace(', ', '\n')}
+            </p>
+            <p>{feed.type === 'Right Breast' ? 'Right' : 'Left'}</p>
+            <p>{Math.floor(feed.length! / 60)}m, {feed.length! % 60}s</p>
+            <div className='flex gap-2 items-center text-[20px]'>
+              <button className='rounded-md bg-secondary p-1'>
+                <FaPen />
+              </button>
+              <form action={handleDeleteFeed}>
                 <input type='hidden' name='id' value={feed.id} />
                 <button className='rounded-md bg-[#F20] p-1'>
                   <FaTrashAlt />
                 </button>
               </form>
-            </li>
-          ))}
-        </ul>
-      </section>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
